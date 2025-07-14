3a7886ed0044558dc027be068f24782870f66a0f
Willy Everton S. Nascimento <willy.nascimento@squadra.com.br>
2025-07-13 23:39:44 -0300

    ignored files

--- services/enrichment-service-node/src/infrastructure/messaging/messaging.module.ts
+++ services/enrichment-service-node/src/infrastructure/messaging/messaging.module.ts
@@ -3,10 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { RabbitMQService } from './rabbitmq/rabbitmq.service';
 import { UserCreatedConsumer } from './rabbitmq/user-created.consumer';
 import { ResilienceModule } from '../resilience/resilience.module';
+import { INJECTION_TOKENS } from '../../domain/constants/injection-tokens';
 
 @Module({
   imports: [ConfigModule, ResilienceModule],
-  providers: [RabbitMQService, UserCreatedConsumer],
-  exports: [RabbitMQService, UserCreatedConsumer],
+  providers: [
+    RabbitMQService,
+    UserCreatedConsumer,
+    {
+      provide: INJECTION_TOKENS.SERVICES.MESSAGE_HANDLER,
+      useClass: RabbitMQService,
+    },
+  ],
+  exports: [RabbitMQService, UserCreatedConsumer, INJECTION_TOKENS.SERVICES.MESSAGE_HANDLER],
 })
 export class MessagingModule { }
